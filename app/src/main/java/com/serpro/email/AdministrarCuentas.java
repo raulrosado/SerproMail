7410f1d008f427eb4527dce3f4f72d0f325f5c8a
Raul Remedios Rosado <raulrosado91@gmail.com>
2020-05-26 17:22:05 -0400

    se agrego las actividades de setting y addcuenta y se creo la bd y la table de las cuentas

--- app/src/main/java/com/serpro/email/AdministrarCuentas.java
+++ app/src/main/java/com/serpro/email/AdministrarCuentas.java
@@ -1,19 +1,66 @@
 package com.serpro.email;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+import com.google.android.material.dialog.MaterialDialogs;
 
 public class AdministrarCuentas extends AppCompatActivity {
+    ConexionSQLiteHelper conn;
+    LinearLayout addNewCuenta;
+    CardView cardNewCuenta;
+    Button btnAdddCuenta;
+    EditText edtnombre,edtemail,edtpassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_administrar_cuentas);
+
+        conn = new ConexionSQLiteHelper(getApplicationContext());//conexion  con la bd
+
+        addNewCuenta = findViewById(R.id.addNewCuenta);
+        cardNewCuenta = findViewById(R.id.cardNewCuenta);
+        btnAdddCuenta = findViewById(R.id.btnAdddCuenta);
+        edtnombre = findViewById(R.id.edtnombre);
+        edtemail = findViewById(R.id.edtemail);
+        edtpassword = findViewById(R.id.edtpassword);
+
+        addNewCuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("mail","el dialog");
+                cardNewCuenta.setVisibility(View.VISIBLE);
+            }
+        });
+
+        btnAdddCuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!edtnombre.toString().isEmpty() || !edtemail.toString().isEmpty()|| !edtpassword.toString().isEmpty()) {
+                    if (Funciones.isValidEmailAddress(edtemail.toString())) {
+                        conn.addCuenta(edtnombre.toString(), edtemail.toString(), edtpassword.toString());
+                        Toast.makeText(AdministrarCuentas.this, "Se agrego la cuenta", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
     }
 
     @Override public boolean onCreateOptionsMenu(Menu menu){
@@ -40,4 +87,5 @@ public class AdministrarCuentas extends AppCompatActivity {
                 return super.onOptionsItemSelected(option_menu);
         }
     }
+
 }
